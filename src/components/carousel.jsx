1ee1c41aa2edb996e010470fc18c1b361b6f78d7
shivaonkruger <shiva.ghubs@gmail.com>
2025-07-17 16:00:25 +0530

    carousel adjustments which still dont render

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,33 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-
-export default function carousel() {
-   
+function Autoplay() {
+    const [slider,setslider]=usestate([
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"},
+        {img: "https://staging.design-mumbai.com/wp-content/uploads/2025/06/Design-Hunger-1.svg"}
+    ])
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 8,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     speed: 2000,
     autoplaySpeed: 2000,
     cssEase: "linear"
-  }
+  };
   return (
-    <div className="slider-container w-full flex px-[38px] py-[22px] gap 5">
+    <div className="slider-container">
       <Slider {...settings}>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
-        <div className="brandimages caret-amber-200 w-[128px] h-[95px]"><img src="https://staging.design-mumbai.com/wp-content/uploads/2024/07/park-hotel_logo.svg" alt="logo" /></div>
+        {slider.map((item)=>
+            <div>
+             <img src={slider.img} alt="" className="w-[128px] h-[95px]" />
+            </div>)}
       </Slider>
     </div>
+   
   );
 }
+
+export default Autoplay;
